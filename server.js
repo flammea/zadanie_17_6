a086fb3291ae5ecf6a5278dadb0a4f9bb52430be
Beata Markunas <beata.markunas@gmail.com>
2018-05-07 07:16:31 +0200

    Create second-template. Add styles

--- server.js
+++ server.js
@@ -4,24 +4,25 @@ var app = express();
 app.set('view engine', 'pug');
 app.set('views','./views');
 
-app.use('/store', function(req, res, next){
-    console.log('Jestem pośrednikiem przy żądaniu do /store.');
+app.use(express.static('assets'));
+
+app.use('/auth/google', function(req, res, next){
+    console.log('Jestem pośrednikiem przy żądaniu do /auth/google.');
     next();
 });
 
 
-app.get('/', function(req, res) {
-    res.send('Hello world!');
-});
-
-app.get('/store', function(req, res) {
-    res.send('To jest sklep');
-});
-
-app.get('/first-template', function(req, res){
+app.get('/auth/google', function(req, res){
     res.render('first-template');
 });
 
+
+app.get('/succeed/login', function (req, res) {
+	res.render('second-template', {
+    username: req.query.login
+  })
+});
+    
 app.listen(3000);
 app.use(function (req, res, next) {
     res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
